5b0fb00ae39a8839e7c11a1cf0e82a590bb03176
Gamage Hashan <tharinduh20@gmail.com>
2025-02-17 14:09:37 +0530

    update the model and controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,16 +6,16 @@ import User from "../models/User";
 const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey";
 
 export const register = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
+    const { username,email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword });
+    const newUser = new User({ username,email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: "User registered successfully!" });
   };
   
   export const login = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username: username }); 
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email}); 
     if (!user) return res.status(404).json({ message: "User not found" });
   
     const isMatch = await bcrypt.compare(password, user.password);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+}, { timestamps: true });
 
 export default mongoose.model("User", UserSchema);
+
